8bb080132bdb100d9231570921ba0ab89cd8edab
takero8538-lgtm <takero8538@gmail.com>
2025-11-25 16:29:03 +0900

    Update RectResize.js

--- src/components/Canvas/rect/RectResize.js
+++ src/components/Canvas/rect/RectResize.js
@@ -22,7 +22,10 @@ export function renderResize(svg, rect, label, item, g, onUpdate) {
     g.appendChild(handle);
 
     handle.onpointerdown = (e) => {
+      // ★ スクロール誤発火防止
+      e.preventDefault();
       e.stopPropagation();
+
       selectItem(state, item.id, onUpdate);
       let { x: lastX, y: lastY } = getSvgPoint(e, svg);
       let pending = false;
